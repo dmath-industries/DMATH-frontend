14e53ada0c15736ee89c99f56072b128252c57f1
zevtos <409315@edu.itmo.ru>
2025-11-11 11:45:00 +0300

    remove deprecated files

--- src/components/__tests__/Title.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Title from '../Title';
-
-test('renders title text', () => {
-  render(<Title text="DMath" />);
-  expect(screen.getByRole('heading', { name: 'DMath' })).toBeInTheDocument();
-});
