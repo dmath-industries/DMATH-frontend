be73c0da33bb815fe8f427382a11cf4b41385864
Makhmudova Maria <maria-06112004@mail.ru>
2025-12-03 16:40:36 +0300

    test: DM-30 add tests for Ford-Bellman algorithm step generator

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,7 @@ module.exports = {
       displayName: 'jsdom',
       testEnvironment: 'jsdom',
       setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-      testMatch: ['<rootDir>/src/**/*.browser.test.ts?(x)'],
+      testMatch: ['<rootDir>/src/**/*.browser.test.ts?(x)', '<rootDir>/src/**/__tests__/**/*.test.ts'],
       collectCoverageFrom,
       transform: { '^.+\\.tsx?$': tsTransformReact },
       moduleNameMapper: {
