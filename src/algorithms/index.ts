b3dd1b5d48c14bb5613126288a360aa24d5c3d3c
zevtos <409315@edu.itmo.ru>
2025-12-07 15:53:05 +0300

    feat: DMATH-27 prim page and test

--- src/algorithms/index.ts
+++ src/algorithms/index.ts
@@ -3,4 +3,5 @@
  * Экспорт всех алгоритмов для работы с графами
  */
 
-export { RobertsFloresStepGenerator } from './roberts-flores/steps'; 
+export { RobertsFloresStepGenerator } from './roberts-flores/steps';
+export { PrimStepGenerator } from './prim/steps';
