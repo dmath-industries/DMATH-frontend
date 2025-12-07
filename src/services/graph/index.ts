bf85ac393158e353f5f4961f40573cc0d4c79c31
zevtos <409315@edu.itmo.ru>
2025-12-07 14:54:25 +0300

    fix: DMATH-19 rebase dev

--- src/services/graph/index.ts
+++ src/services/graph/index.ts
@@ -5,6 +5,3 @@
 export { GraphModel } from './GraphModel';
 export { LayoutService } from './LayoutService';
 export type { LayoutOptions } from './LayoutService';
-export { graphDTOToGraphology } from './GraphUtils';
-export type { GraphConversionResult } from './GraphUtils';
-
