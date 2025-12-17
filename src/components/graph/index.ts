926d74c33ae70f6037bfcc9376a6c8cf55663647
Makhmudova Maria <maria-06112004@mail.ru>
2025-12-17 16:38:22 +0300

    refactor: DMATH-42 integrate explanation generation into Bellman-Ford step generator

--- src/components/graph/index.ts
+++ src/components/graph/index.ts
@@ -2,6 +2,7 @@ export { GraphContainer } from './GraphContainer';
 export { GraphCanvas } from './GraphCanvas';
 export { GraphEditor } from './GraphEditor';
 export { ControlPanel } from './ControlPanel';
+export { StepExplanationPanel } from './StepExplanationPanel';
 export { AlgorithmPanel } from './AlgorithmPanel';
 export { ViewportControls } from './ViewportControls';
 export { AlgorithmLayout, useAlgorithmLayout } from './AlgorithmLayout';
